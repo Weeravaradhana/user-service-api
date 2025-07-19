33bb3312b32a2546efd91ee778b3f0c121931002
Weeravaradhana <thaminduweeravardana@gmail.com>
2025-07-19 11:51:46 +0530

    Create user controller and user service classes.

---
+++ src/main/java/com/dewstack/whilecart/user_service_api/api/UserController.java
@@ -0,0 +1,4 @@
+package com.dewstack.whilecart.user_service_api.api;
+
+public class UserController {
+}
---
+++ src/main/java/com/dewstack/whilecart/user_service_api/service/UserService.java
@@ -0,0 +1,4 @@
+package com.dewstack.whilecart.user_service_api.service;
+
+public interface UserService {
+}
